86061a97e38139be7fca797edf8b64717337bb14
tigerager <afifnusada@gmail.com>
2021-03-10 12:31:56 +0700

    initial commit

---
+++ conf/conn.php
@@ -0,0 +1,4 @@
+<?php
+$db=mysqli_connect('localhost','root','','cv') or die('Connect Failed !!!');
+
+?>
---
+++ pages/beranda.php
@@ -0,0 +1,23 @@
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1 class="text-primary">
+        <i class="fa fa-dashboard"></i> CV
+        
+      </h1>
+      <ol class="breadcrumb">
+        <li class="active"><i class="fa fa-dashboard"></i> BERANDA</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+    <img src="cv_AFIFRAHMATNUSADA.PNG" height="1000" width="700" >
+    <!-- row -->
+    <div class="row">
+        
+    </div>
+    </section>
+    <!-- /.Main content -->
+  </div>
+<!-- /.content-wrapper -->
